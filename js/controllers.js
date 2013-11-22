1afb9587c5f016aab5da324640416c54e8119892
Aaron Roe <aaronalkan@gmail.com>
2013-11-21 21:31:16 -0600

    Moves map code into the searchCtrl controller.

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,16 @@
 var mapApp = angular.module('mapApp', []);
 
 mapApp.controller('SearchCtrl', function($scope, $http, $window) {
-    $scope.$window = $window;
+    // init function for body.
+    $scope.init = function() {
+        initializeMap();
+    };
 
     // declare Fuse searcher
     var searcher;
+
+    // declare the google map.
+    var map;
 
     //init search results.
     $scope.searchResults = [];
@@ -27,6 +33,8 @@ mapApp.controller('SearchCtrl', function($scope, $http, $window) {
           keys: ['name']
         }
         searcher = new Fuse(result.data, options);
+
+        // add markers to the map.
     });
 
     $scope.$watch('searchText', function(newValue, oldValue) {
@@ -34,5 +42,20 @@ mapApp.controller('SearchCtrl', function($scope, $http, $window) {
             $scope.searchResults = searcher.search(newValue);
         }
     });
+
+    /**
+     * Add code for initializing the map.
+     */
+    function initializeMap() {
+        var mapOptions = {
+          zoom: 17,
+          center: new google.maps.LatLng(29.718204, -95.400000),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+
+
+    }
 
 });
