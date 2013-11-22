7bfe43db6b98c1aaf500716d7194432a5eadfbc5
Aaron Roe <aaronalkan@gmail.com>
2013-11-21 20:53:03 -0600

    Adds style to search results and adds click behavior when it is open.

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,25 @@
 var mapApp = angular.module('mapApp', []);
 
-mapApp.controller('SearchCtrl', function($scope, $http) {
+mapApp.controller('SearchCtrl', function($scope, $http, $window) {
+    $scope.$window = $window;
+
     // declare Fuse searcher
     var searcher;
 
+    //init search results.
     $scope.searchResults = [];
+
+    // init the open status for the search results.
+    $scope.open = true;
+
+    $scope.hideSearchResults = function(clickedElement) {
+        if (clickedElement.target.id !== 'searchBox') {
+            $scope.open = false;
+        }
+        else {
+            $scope.open = true;
+        }
+    }
 
     $http.get('/data/campus_data.json').then(function(result) {
         // set the fuse searcher.
