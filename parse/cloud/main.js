c8720f0caa1ed18c3f331ac6214f6779754bd88b
Waseem Ahmad <waseem@rice.edu>
2014-06-14 01:07:06 -0400

    Implemented rudimentary search, doesn't quite work

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -4,3 +4,26 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("placesSearch", function(request, response) {
+  var query = new Parse.Query("Place");
+
+  // Ensure that each token in the query string is contained within
+  // the keywords field
+  var tokens = request.params.query.split(' ');
+  console.log(tokens);
+  for (var i = 0; i < tokens.length; ++i) {
+    // TODO: Change 'name' to 'keywords' once keywords are populated
+    // TODO Fix problem, only last token gets added as a constraint
+    query.contains("name", tokens[i]);
+  }
+
+  query.find({
+    success: function(results) {
+      response.success(results);
+    },
+    error: function(error) {
+      response.error(error);
+    }
+  });
+});
