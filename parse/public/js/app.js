05bc860ddcdf8c52b244e3319d3a0d27e7c1157f
Waseem Ahmad <waseem@rice.edu>
2014-07-20 12:25:52 -0400

    Prod / Dev fork

--- parse/public/js/app.js
+++ parse/public/js/app.js
@@ -14,10 +14,18 @@ atlasApp.config(function($routeProvider) {
 });
 
 atlasApp.run(function($rootScope) {
-  Parse.initialize(
-    "FpiNAPH6LROJqpuYHyIG7X1xgQiLcGjECxWZU2ys",
-    "dajAbPrS1zLC8bkvJQ77fXb6C7RfbYbu6uZbUb10"
-  );
+  var prod = true;
+  if (prod) {
+    Parse.initialize(
+      "Hs7BvAGhMU9B4g757NZ0YAT0kOtxQ1hM78bQGu2y",
+      "4jd4UMTQdExYgXgh40G7gppWnQgJB2mz7F3UE5ik"
+    );
+  } else {
+    Parse.initialize(
+      "FpiNAPH6LROJqpuYHyIG7X1xgQiLcGjECxWZU2ys",
+      "dajAbPrS1zLC8bkvJQ77fXb6C7RfbYbu6uZbUb10"
+    );
+  }
 
 });
 
