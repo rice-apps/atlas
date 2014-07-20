79849fb00368ec9f3bf33132ef1a1f3580e08d33
Waseem Ahmad <waseem@rice.edu>
2014-07-19 23:33:09 -0400

    Hooked up typeahead to Places Search cloud function

--- parse/public/js/app.js
+++ parse/public/js/app.js
@@ -1,16 +1,24 @@
 'use strict';
 
-var atlasApp = angular.module('atlasApp', ['ngRoute']);
+var atlasApp = angular.module('atlasApp', ['ngRoute', 'ui.bootstrap']);
 
 atlasApp.config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: '/views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  $routeProvider
+    .when('/', {
+      templateUrl: '/views/main.html',
+      controller: 'MainCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+atlasApp.run(function($rootScope) {
+  Parse.initialize(
+    "FpiNAPH6LROJqpuYHyIG7X1xgQiLcGjECxWZU2ys",
+    "dajAbPrS1zLC8bkvJQ77fXb6C7RfbYbu6uZbUb10"
+  );
+
+});
 
 console.log('Atlas App loaded.');
--- parse/public/js/controllers/main.js
+++ parse/public/js/controllers/main.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('atlasApp')
-  .controller('MainCtrl', function($scope) {
+  .controller('MainCtrl', function($scope, $http, $q) {
     console.log('Main Controller Initialized');
+
+    $scope.searchPlaces = function(query) {
+      return Parse.Cloud.run("placesSearch", {query: query});
+    };
+
   });
