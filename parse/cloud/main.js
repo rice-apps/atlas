8bd8a0817acd5b6966130467b476cde02e98e21c
Waseem Ahmad <waseem@rice.edu>
2014-07-23 00:39:23 -0400

    Created separate cloud function for autocomplete

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,10 +1,11 @@
 require('cloud/app.js');
 var Fuse = require('cloud/fuse.min.js');
 
+
 /*
  * Provides Cloud Functions for Rice Maps.
  */
-Parse.Cloud.define("placesSearch", function(request, response) {
+Parse.Cloud.define("placeSearch", function(request, response) {
   console.log("Search Query: " + request.params.query);
 
   // Define Parse cloud query that retrieves all Place objects and matches them to a search
@@ -28,3 +29,32 @@ Parse.Cloud.define("placesSearch", function(request, response) {
     }
   });
 });
+
+/*
+ * Provides autocomplete suggestions for Places 
+ */
+Parse.Cloud.define("placeAutocomplete", function(request, response) {
+  console.log("Search Query: " + request.params.query);
+
+  // Define Parse cloud query that retrieves all Place objects and matches them to a search
+  var query = new Parse.Query("Place");
+  query.limit(200);
+  query.include("parentPlace");
+  query.select("name");
+
+  query.find({
+    success: function(results) {
+      // Perform Fuse.js fuzzy search on all Place objects
+      var options = {
+        keys: ['attributes.name', 'attributes.symbol']
+      }
+
+      var searcher = new Fuse(results, options);
+      var matches = searcher.search(request.params.query);
+      response.success(matches.slice(0, 10));
+    },
+    error: function(error) {
+      response.error(error);
+    }
+  });
+})
--- parse/public/js/controllers/main.js
+++ parse/public/js/controllers/main.js
@@ -4,16 +4,8 @@ angular.module('atlasApp')
   .controller('MainCtrl', function($scope, $http, $q) {
     console.log('Main Controller Initialized');
 
-    $scope.searchPlaces = function(query) {
-      var deferred = $q.defer();
-
-      Parse.Cloud.run("placesSearch", {query: query}).then(function(res) {
-        console.log(res);
-        window.res = res;
-        deferred.resolve(res);
-      });
-
-      return deferred.promise;
+    $scope.placeAutocomplete = function(query) {
+      return Parse.Cloud.run("placeAutocomplete", {query: query});
     };
 
   });
