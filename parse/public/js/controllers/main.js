662a98b4530e460d2e641e46cddf4a33429354ed
Waseem Ahmad <waseem@rice.edu>
2014-07-27 21:05:10 -0400

    Search results are displayed in JSON

--- parse/public/js/controllers/main.js
+++ parse/public/js/controllers/main.js
@@ -4,8 +4,28 @@ angular.module('atlasApp')
   .controller('MainCtrl', function($scope, $http, $q) {
     console.log('Main Controller Initialized');
 
-    $scope.placeAutocomplete = function(query) {
+    $scope.searchResults = [];
+
+    $scope.autocomplete = function(query) {
       return Parse.Cloud.run("placeAutocomplete", {query: query});
     };
+
+    $scope.search = function(query) {
+      console.log("Performing search");
+      Parse.Cloud.run("placeSearch", {query: query}).then(function(res) {
+        $scope.$apply(function() {
+          $scope.searchResults = res;
+          console.log("Updated $scope.searchResults");
+          console.log($scope.searchResults);
+        });
+      });
+    };
+
+    $scope.searchEnter = function(keyEvent, query) {
+      if (keyEvent.which == 13) { // Enter pressed
+        console.log("Enter pressed");
+        $scope.search(query);
+      }
+    }
 
   });
