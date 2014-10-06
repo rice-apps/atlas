540211ea4e6a9304fe36cfa4a6c25a0c0acf5332
John King <johnfrancisking@gmail.com>
2014-10-05 22:47:20 -0500

    Refactor MapsService to use proper object notation and change map initialization to happen once

--- parse/public/js/MapsService.js
+++ parse/public/js/MapsService.js
@@ -2,55 +2,83 @@ angular.module('atlasApp').service('MapsService', function(
 	$scope, 
 	$http
 ) {
-    return {
+	var MapsService = {};
 
-    	var self = this;
-        /**
-	    * Initializes the Google Maps canvas
-	    */
-	    initMap: function () {
-		    console.log('Initializing');
-		    var mapOptions = {
-		      zoom: 15,
-		      center: $scope.mapCenter,
-		      mapTypeId: google.maps.MapTypeId.ROADMAP,
-		      disableDefaultUI: true,
-		    };
+	MapsService.mapCenter = new google.maps.LatLng(29.717384, -95.403171);
 
-		    var mapCanvas = document.getElementById('map-canvas');
-
-		    self.map = new google.maps.Map(
-		      mapCanvas,
-		      mapOptions
-		    );
+    /**
+    * Initializes the Google Maps canvas
+    */
+    MapsService.initMap = function () {
+	    console.log('Initializing');
+	    var mapOptions = {
+	      zoom: 15,
+	      center: self.mapCenter,
+	      mapTypeId: google.maps.MapTypeId.ROADMAP,
+	      disableDefaultUI: true,
 	    };
 
+	    MapsService.mapCanvas = document.getElementById('map-canvas');
 
-	    plotMarker: function(lat, lng, name) {
-		    var position = new google.maps.LatLng(lat, lng);
+	    MapsService.map = new google.maps.Map(
+	      mapCanvas,
+	      mapOptions
+	    );
+    };
 
-		    self.marker = new google.maps.Marker({
-		      position: position,
-		      map: $scope.map,
-		      title: name
-		    });
-		    self.map.setCenter(position);
-		};
-
-		/**
-		 * Resizes the view to fit within the bounds of the screen.
-		 */
-		resizeView: function() {
-			var newHeight = 
-			$(window).height() 
-				- $('div.navbar').height() 
-				- 90
-				- $('#toolbar').height();
-			$('#map-canvas').css({height: newHeight});
-		};
+     MapsService.showMap = function() {
+	    MapsService.mapCanvas.hidden = false;
+	 };
+	  
+	 MapsService.hideMap = function() {
+	    MapsService.mapCanvas.hidden = true;
+	 };
 
 
-		clearMap: function() {};
+    MapsService.plotMarker = function(lat, lng, name) {
+	    var position = new google.maps.LatLng(lat, lng);
 
+	    MapsService.marker = new google.maps.Marker({
+	      position: position,
+	      map: MapsService.map,
+	      title: name
+	    });
+
+	    return position;
+	};
+
+	/**
+	 * Resizes the view to fit within the bounds of the screen.
+	 */
+	MapsService.resizeView = function() {
+		var newHeight = 
+		$(window).height() 
+			- $('div.navbar').height() 
+			- 90
+			- $('#toolbar').height();
+		$('#map-canvas').css({height: newHeight});
+	};
+
+
+	MapsService.setCenter = function(position) {
+	    MapsService.map.setCenter(position);
+	};
+
+
+	MapsService.clearMap = function() {};
+
+
+	MapsService.getMap = function() {
+		return MapsService.map;
+	}
+
+
+    return {
+		initMap: MapsService.initMap,
+	    showMap: MapsService.showMap,
+	    hideMap: MapsService.hideMap,
+	    plotMarker: MapsService.plotMarker,
+	    setCenter: MapsService.setCenter,
+	    getMap: MapsService.getMap
     };
 });
