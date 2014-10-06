d779986448519a3837826c630ec2f214f6f10464
Waseem Ahmad <waseemahmad@fb.com>
2014-10-05 17:37:04 -0700

    #17203893586555: Removed debugging statement

--- parse/public/js/controllers/main.js
+++ parse/public/js/controllers/main.js
@@ -4,7 +4,6 @@ angular.module('atlasApp').controller('MainCtrl', function(
     $routeParams,
     $scope) {
   $scope.searchResults = [];
-  window.searchResults = $scope.searchResults;
   if ($routeParams.q) {   // If url contains search query, perform search
     $scope.query = $routeParams.q;
   }
