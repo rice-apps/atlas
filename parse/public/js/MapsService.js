cdc20628444cfe6b5f21d245220fcd13d57bd186
Waseem Ahmad <waseemahmad@fb.com>
2014-10-05 21:30:28 -0700

    #16772395038316: Changed tabs to spaces

--- parse/public/js/MapsService.js
+++ parse/public/js/MapsService.js
@@ -1,84 +1,83 @@
 angular.module('atlasApp').service('MapsService', function(
-	$scope, 
-	$http
+  $scope, 
+  $http
 ) {
-	var MapsService = {};
+  var MapsService = {};
 
-	MapsService.mapCenter = new google.maps.LatLng(29.717384, -95.403171);
+  MapsService.mapCenter = new google.maps.LatLng(29.717384, -95.403171);
 
-    /**
-    * Initializes the Google Maps canvas
-    */
-    MapsService.initMap = function () {
-	    console.log('Initializing');
-	    var mapOptions = {
-	      zoom: 15,
-	      center: self.mapCenter,
-	      mapTypeId: google.maps.MapTypeId.ROADMAP,
-	      disableDefaultUI: true,
-	    };
-
-	    MapsService.mapCanvas = document.getElementById('map-canvas');
-
-	    MapsService.map = new google.maps.Map(
-	      mapCanvas,
-	      mapOptions
-	    );
+  /**
+  * Initializes the Google Maps canvas
+  */
+  MapsService.initMap = function () {
+    console.log('Initializing');
+    var mapOptions = {
+      zoom: 15,
+      center: self.mapCenter,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true,
     };
 
-     MapsService.showMap = function() {
-	    MapsService.mapCanvas.hidden = false;
-	 };
-	  
-	 MapsService.hideMap = function() {
-	    MapsService.mapCanvas.hidden = true;
-	 };
+    MapsService.mapCanvas = document.getElementById('map-canvas');
+
+    MapsService.map = new google.maps.Map(
+      mapCanvas,
+      mapOptions
+    );
+  };
+
+  MapsService.showMap = function() {
+    MapsService.mapCanvas.hidden = false;
+  };
+    
+  MapsService.hideMap = function() {
+    MapsService.mapCanvas.hidden = true;
+  };
 
 
-    MapsService.plotMarker = function(lat, lng, name) {
-	    var position = new google.maps.LatLng(lat, lng);
+  MapsService.plotMarker = function(lat, lng, name) {
+    var position = new google.maps.LatLng(lat, lng);
 
-	    MapsService.marker = new google.maps.Marker({
-	      position: position,
-	      map: MapsService.map,
-	      title: name
-	    });
+    MapsService.marker = new google.maps.Marker({
+      position: position,
+      map: MapsService.map,
+      title: name
+    });
 
-	    return position;
-	};
+    return position;
+  };
 
-	/**
-	 * Resizes the view to fit within the bounds of the screen.
-	 */
-	MapsService.resizeView = function() {
-		var newHeight = 
-		$(window).height() 
-			- $('div.navbar').height() 
-			- 90
-			- $('#toolbar').height();
-		$('#map-canvas').css({height: newHeight});
-	};
-
-
-	MapsService.setCenter = function(position) {
-	    MapsService.map.setCenter(position);
-	};
+  /**
+   * Resizes the view to fit within the bounds of the screen.
+   */
+  MapsService.resizeView = function() {
+    var newHeight = 
+    $(window).height() 
+      - $('div.navbar').height() 
+      - 90
+      - $('#toolbar').height();
+    $('#map-canvas').css({height: newHeight});
+  };
 
 
-	MapsService.clearMap = function() {};
+  MapsService.setCenter = function(position) {
+    MapsService.map.setCenter(position);
+  };
 
 
-	MapsService.getMap = function() {
-		return MapsService.map;
-	}
+  MapsService.clearMap = function() {};
+
+  MapsService.getMap = function() {
+    return MapsService.map;
+  }
 
 
-    return {
-		initMap: MapsService.initMap,
-	    showMap: MapsService.showMap,
-	    hideMap: MapsService.hideMap,
-	    plotMarker: MapsService.plotMarker,
-	    setCenter: MapsService.setCenter,
-	    getMap: MapsService.getMap
-    };
+  return {
+    initMap: MapsService.initMap,
+    showMap: MapsService.showMap,
+    hideMap: MapsService.hideMap,
+    plotMarker: MapsService.plotMarker,
+    setCenter: MapsService.setCenter,
+    getMap: MapsService.getMap
+  };
 });
