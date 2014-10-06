bf6145b63ec42c1c75a05a72c55322cfdd737157
Waseem Ahmad <waseemahmad@fb.com>
2014-10-05 17:33:25 -0700

    #17203893586555: Created SearchBar directive and swapped it out everywhere

--- parse/public/js/controllers/main.js
+++ parse/public/js/controllers/main.js
@@ -1,59 +1,11 @@
 'use strict';
 
 angular.module('atlasApp').controller('MainCtrl', function(
-  $location,
-  $routeParams,
-  $scope,
-  $http,
-  $q,
-  $analytics,
-  cfpLoadingBar
-) {
-
+    $routeParams,
+    $scope) {
   $scope.searchResults = [];
-
-  /**
-   * Provides autocomplete suggestions as you type the query
-   */
-  $scope.autocomplete = function(query) {
-    var places = Parse.Cloud.run("placeAutocomplete", {query: query});
-    return places;
-    // var courses = Parse.Cloud.run("courseAutocomplete", {query: query});
-    // return places + courses
-  };
-
-  /**
-   * Google Analytics for Autocomplete
-   */
-  $scope.onAutocompleteSelect = function(item, model, label, from) {
-    $analytics.eventTrack(
-      'Click Autocomplete Suggestion from ' + from,
-      { category: 'Search' }
-    );
-    $scope.search(model.id);
-  }
-  
-  /**
-   * Used to request a search with the provided query
-   */
-  $scope.search = function(query) {
-    $location.url('/place/' + query);
-  }
-
-  /**
-   * Performs the actual search and provides the results
-   */
-  $scope._search = function(query) {
-    cfpLoadingBar.start();
-    Parse.Cloud.run("placeSearch", {query: query}).then(function(res) {
-      $scope.searchResults = res;
-      cfpLoadingBar.complete();
-    });
-  };
-
+  window.searchResults = $scope.searchResults;
   if ($routeParams.q) {   // If url contains search query, perform search
     $scope.query = $routeParams.q;
-    $scope._search($routeParams.q);
   }
-
 });
